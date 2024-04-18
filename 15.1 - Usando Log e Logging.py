6bb71d926e7c2a92d8a0beb707835d9e5745dbd1
taciofag <123953908+taciofag@users.noreply.github.com>
2024-04-18 18:00:26 -0300

    Logging

---
+++ 15.1 - Usando Log e Logging.py
@@ -0,0 +1,24 @@
+# Logging
+
+import logging
+import datetime
+import sys
+
+agora = datetime.datetime.now()
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log',encoding='utf-8',filemode='a',format='%(levelname)s - %(message)s') # Setar o nível
+email = input('Digite seu email: ')
+while True:
+    try:
+            
+        senha = int(input('Digite sua senha: '))
+        if senha == 123456:
+            print('Acesso Concedido')
+            logging.info(f'{email} entrou em sua conta bancária {agora.strftime("%Y-%m-%d %H:%M:%S")}')
+            break
+        else:
+            print('Senha incorreta, digite novamente')
+            logging.info(f'{email} errou a senha {agora.strftime("%Y-%m-%d %H:%M:%S")}')
+    except ValueError as erro:
+        print(f'Digite apenas números')
+        logging.info(f'Não digitou números inteiros às {agora.strftime("%Y-%m-%d %H:%M:%S")}')
