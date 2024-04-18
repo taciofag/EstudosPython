7d6ae699e6541b536a15921498f8674027037f7b
taciofag <123953908+taciofag@users.noreply.github.com>
2024-04-18 17:12:06 -0300

    Logging

--- 15 - Log e Logging.py
+++ 15 - Log e Logging.py
@@ -1,2 +1,20 @@
 # Logging
 
+import logging
+'''
+debug
+info
+warning
+error
+critical
+'''
+
+logging.basicConfig(level=logging.DEBUG,filename='app.log',filemode='a',format='%(levelname)s - %(message)s') # Setar o nível
+logging.debug('Logging nivel debug')
+logging.info('Logging nivel info')
+logging.warning('Logging nivel warning')
+logging.error('Logging nivel error')
+logging.critical('Logging nivel critico')
+
+# guardar informações no log
+
