e77260848b11653889d5e54cda0bcc1c2c1d3541
Tácio Fagundes <tacio.fagundes2@gmail.com>
2024-04-30 13:42:49 -0300

    classes

---
+++ Aulas/21 - Aula21/21 - app.py
@@ -0,0 +1,14 @@
+import os
+
+print(os.name())
+print(os.listdir())
+print(os.getcwd())
+print(os.sep)
+print(os.path.join(os.getcwd() + os.sep + 'senhas.txt'))
+print(os.path.join(os.getcwd() + os.sep + 'Documentos' + os.sep + 'musica.mp3'))
+
+caminho_senha = (os.path.join(os.getcwd() + os.sep + 'senhas.txt'))
+
+print(os.path.dirname(caminho_senha)) #dirname = nome do diretório de um path
+print(os.path.basename(caminho_senha)) #basename = nome do arquivo de um path
+print(os.path.exists(os.getcwd() + os.sep + 'Aulas'))
